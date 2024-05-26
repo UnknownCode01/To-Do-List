9253743add1a165f8c8c0c73459a2966297dc028
Brijit Adak <brijitadak000@gmail.com>
2024-05-26 20:34:54 +0530

    add

--- src/components/ToDoItem.jsx
+++ src/components/ToDoItem.jsx
@@ -1,17 +1,15 @@
-import React, { useState } from "react";
+import React from "react";
 
-function TodoItem(props) {
-  const [isDone, setIsDone] = useState(false);
-  function handleClick() {
-    setIsDone(!isDone);
-  }
+function ToDoItem(props) {
   return (
-    <div onClick={handleClick}>
-      <li style={{ textDecoration: isDone ? "line-through" : null }}>
-        {props.text}
-      </li>
-      ;
+    <div
+      onClick={() => {
+        props.onChecked(props.id);
+      }}
+    >
+      <li>{props.text}</li>
     </div>
   );
 }
-export default TodoItem;
+
+export default ToDoItem;
